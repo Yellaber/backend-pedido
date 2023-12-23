f38019f3103dd84bf893e3d3e501cc21dd98ef6e
Yesid Llamas <yellaber@gmail.com>
2023-12-22 21:19:51 -0500

    Proveedor.java: Se agregaron métodos setters y getters

--- src/main/java/com/yesid/backend/pedido/app/entities/Proveedor.java
+++ src/main/java/com/yesid/backend/pedido/app/entities/Proveedor.java
@@ -13,11 +13,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
 @Entity
 @Table(name="proveedores")
-@Data
 public class Proveedor {
 	
 	@Id
@@ -41,5 +39,57 @@ public class Proveedor {
 	
 	public Proveedor() {
 		this.productos = new ArrayList<Producto>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 }
