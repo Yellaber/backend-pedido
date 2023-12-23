60fc8c32fa84194b94c388c0ce7b19fb3105557d
Yesid Llamas <yellaber@gmail.com>
2023-12-23 13:32:24 -0500

    MesaController.java: Modificado

--- src/main/java/com/yesid/backend/pedido/app/controllers/MesaController.java
+++ src/main/java/com/yesid/backend/pedido/app/controllers/MesaController.java
@@ -13,23 +13,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yesid.backend.pedido.app.entities.Vendedor;
-import com.yesid.backend.pedido.app.services.VendedorService;
+import com.yesid.backend.pedido.app.entities.Mesa;
+import com.yesid.backend.pedido.app.services.MesaService;
 import com.yesid.backend.pedido.app.validations.Validacion;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/vendedor")
-public class VendedorController {
+@RequestMapping("/api/mesa")
+public class MesaController {
 	
 	@Autowired
-	private VendedorService vendedorService;
+	private MesaService mesaService;
 	
 	@GetMapping
 	public ResponseEntity<?> listar() {
 		try {
-			return ResponseEntity.ok(vendedorService.findAll());
+			return ResponseEntity.ok(mesaService.findAll());
 		} catch(Exception ex) {
 			return Validacion.registrarError(ex.getMessage(), HttpStatus.NOT_FOUND);
 		}
@@ -38,31 +38,31 @@ public class VendedorController {
 	@GetMapping("/{id}")
 	public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
 		try {
-			return ResponseEntity.ok(vendedorService.findById(id));
+			return ResponseEntity.ok(mesaService.findById(id));
 		} catch(Exception ex) {
 			return Validacion.registrarError(ex.getMessage(), HttpStatus.NOT_FOUND);
 		}
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> crear(@Valid @RequestBody Vendedor vendedor, BindingResult result) {
+	public ResponseEntity<?> crear(@Valid @RequestBody Mesa mesa, BindingResult result) {
 		try {
 			if(result.hasErrors()) {
 				return Validacion.registrarErrorCampo(result);
 			}
-			return ResponseEntity.status(HttpStatus.CREATED).body(vendedorService.save(vendedor));
+			return ResponseEntity.status(HttpStatus.CREATED).body(mesaService.save(mesa));
 		} catch(Exception ex) {
 			return Validacion.registrarError(ex.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<?> actualizar(@PathVariable Long id, @Valid @RequestBody Vendedor vendedor, BindingResult result) {
+	public ResponseEntity<?> actualizar(@PathVariable Long id, @Valid @RequestBody Mesa mesa, BindingResult result) {
 		try {
 			if(result.hasErrors()) {
 				return Validacion.registrarErrorCampo(result);
 			}
-			return ResponseEntity.status(HttpStatus.CREATED).body(vendedorService.update(id, vendedor));
+			return ResponseEntity.status(HttpStatus.CREATED).body(mesaService.update(id, mesa));
 		} catch(Exception ex) {
 			return Validacion.registrarError(ex.getMessage(), HttpStatus.NOT_FOUND);
 		}
@@ -71,7 +71,7 @@ public class VendedorController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> eliminar(@PathVariable Long id) {
 		try {
-			vendedorService.deleteById(id);
+			mesaService.deleteById(id);
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		} catch(Exception ex) {
 			return Validacion.registrarError(ex.getMessage(), HttpStatus.NOT_FOUND);
