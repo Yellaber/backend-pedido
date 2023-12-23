a83c6d22978592fa94584cfc5c575fc091b73071
Yesid Llamas <yellaber@gmail.com>
2023-12-22 20:48:08 -0500

    Cliente.java: Se agregaron métodos setters y getters

--- src/main/java/com/yesid/backend/pedido/app/entities/Cliente.java
+++ src/main/java/com/yesid/backend/pedido/app/entities/Cliente.java
@@ -14,11 +14,9 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
 @Entity
 @Table(name="clientes")
-@Data
 public class Cliente {
 
 	@Id
@@ -52,5 +50,69 @@ public class Cliente {
 	
 	public Cliente() {
 		this.pedidos = new ArrayList<Pedido>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
 	}
 }
