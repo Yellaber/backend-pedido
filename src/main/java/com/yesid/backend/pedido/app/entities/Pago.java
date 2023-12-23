3bf4c577e36898d8f8725467854062a5a908686f
Yesid Llamas <yellaber@gmail.com>
2023-12-22 21:13:44 -0500

    Pago.java: Se agregaron métodos setters y getters

--- src/main/java/com/yesid/backend/pedido/app/entities/Pago.java
+++ src/main/java/com/yesid/backend/pedido/app/entities/Pago.java
@@ -16,11 +16,9 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import lombok.Data;
 
 @Entity
 @Table(name="pagos")
-@Data
 public class Pago {
 	
 	@Id
@@ -46,5 +44,43 @@ public class Pago {
 	@PrePersist
 	public void prePersist() {
 		this.fecha = new Date();
+	}
+
+	public Pago() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Cuenta getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Cuenta cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
 	}
 }
