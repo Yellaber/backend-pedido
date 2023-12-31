64fe024c7e62cac124aa9f69223a7c0625d71c2e
Yesid Llamas <yellaber@gmail.com>
2023-12-31 14:19:50 -0500

    Cliente.java: Modificado

--- src/main/java/com/yesid/backend/pedido/app/entities/Cliente.java
+++ src/main/java/com/yesid/backend/pedido/app/entities/Cliente.java
@@ -3,6 +3,8 @@ package com.yesid.backend.pedido.app.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -47,9 +49,11 @@ public class Cliente {
 	private String email;
 	
 	@OneToMany(mappedBy="cliente", fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JsonManagedReference(value="pedido_cliente")
 	private List<Pedido> pedidos;
 	
 	@OneToOne(mappedBy="cliente")
+	@JsonManagedReference(value="cuenta_cliente")
 	private Cuenta cuenta;
 	
 	public Cliente() {
