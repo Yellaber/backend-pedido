dc1a02a511b7569343aa44c9e8057be4ffa080c8
Yesid Llamas <yellaber@gmail.com>
2023-12-31 15:56:25 -0500

    IProductoRepository.java: Se agregó consulta JPQL personalizada

--- src/main/java/com/yesid/backend/pedido/app/repositories/IProductoRepository.java
+++ src/main/java/com/yesid/backend/pedido/app/repositories/IProductoRepository.java
@@ -3,6 +3,7 @@ package com.yesid.backend.pedido.app.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.yesid.backend.pedido.app.entities.Producto;
@@ -10,5 +11,6 @@ import com.yesid.backend.pedido.app.entities.Producto;
 @Repository
 public interface IProductoRepository extends JpaRepository<Producto, Long> {
 	
+	@Query("select p from Producto p where lower(p.nombre) like lower(concat(:nombre, '%'))")
 	List<Producto> findByNombreLike(String nombre);	
 }
